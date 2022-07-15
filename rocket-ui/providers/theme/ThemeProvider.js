aecca4eac59936b333fe36cfd3b7289fc7279290
Costi <cdiaconita@totalsoft.ro>
2022-07-15 14:44:28 +0300

    update ThemeProvider

--- rocket-ui/providers/theme/ThemeProvider.js
+++ rocket-ui/providers/theme/ThemeProvider.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ThemeProvider as MUIThemeProvider, StyledEngineProvider } from '@mui/material'
+import { ThemeProvider as MUIThemeProvider } from '@mui/material'
 import { defaultTheme } from '@totalsoft_oss/rocket-ui.themes'
+
+import createCache from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
+
+const muiCache = createCache({
+  key: 'mui',
+  prepend: true
+})
 
 const ThemeProvider = ({ children }) => {
   return (
-    <StyledEngineProvider injectFirst>
+    <CacheProvider value={muiCache}>
       <MUIThemeProvider theme={defaultTheme}>{children}</MUIThemeProvider>
-    </StyledEngineProvider>
+    </CacheProvider>
   )
 }
 

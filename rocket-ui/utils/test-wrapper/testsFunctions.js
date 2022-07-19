134bb416de7115347a0be4ebf9b07f4cb2b3b434
Costi <cdiaconita@totalsoft.ro>
2022-07-19 14:20:38 +0300

    add toast component

--- rocket-ui/utils/test-wrapper/testsFunctions.js
+++ rocket-ui/utils/test-wrapper/testsFunctions.js
@@ -1,9 +1,7 @@
 import { render as baseRender, fireEvent } from '@testing-library/react'
 import TestingWrapper from './TestingWrapper'
-// import { renderHook } from '@testing-library/react-hooks'
 
 export const render = (ui, options) => baseRender(ui, { wrapper: TestingWrapper, ...options })
-// const customRenderHook = (hook, options) => renderHook(hook, { wrapper: TestingWrapper, ...options })
 
 // more comprehensive simulation of a user click (mousedown + click)
 export const userClick = element => {
@@ -11,6 +9,3 @@ export const userClick = element => {
   fireEvent.mouseUp(element)
   element.click()
 }
-
-// export * from '@testing-library/react'
-/*, customRenderHook as renderHook*/

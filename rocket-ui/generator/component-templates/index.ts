3b75bc125815d25b14d2a2c087b52e8497bae093
Costi <cdiaconita@totalsoft.ro>
2022-07-21 12:09:39 +0300

    add custom component generator

---
+++ rocket-ui/generator/component-templates/index.ts
@@ -0,0 +1,5 @@
+import { ComponentTemplatesAspect } from './component-templates.aspect';
+
+export type { ComponentTemplatesMain } from './component-templates.main.runtime';
+export default ComponentTemplatesAspect;
+export { ComponentTemplatesAspect };

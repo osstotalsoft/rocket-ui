862db796fa6b2dc9ff765beb75efd0a81675c073
Costi <cdiaconita@totalsoft.ro>
2022-10-14 19:33:00 +0300

    fix toast style

--- rocket-ui/components/feedback/toast/ToastStyles.js
+++ rocket-ui/components/feedback/toast/ToastStyles.js
@@ -1,6 +1,6 @@
-import { styled } from "@mui/material/styles";
+import { styled } from '@mui/material/styles'
 
-const PREFIX = "StyledToast";
+const PREFIX = 'StyledToast'
 
 export const classes = {
   default: `${PREFIX}-default`,
@@ -9,39 +9,49 @@ export const classes = {
   error: `${PREFIX}-error`,
   warning: `${PREFIX}-warning`,
   toastWrapper: `${PREFIX}-toastWrapper`
-};
+}
 
 const Container = styled('div')(({ theme }) => ({
-  [`& .${classes.default}`]: { borderRadius: "6px", padding: "6px 20px" },
+  [`& .${classes.default}`]: { borderRadius: '6px', padding: '6px 20px' },
   [`& .${classes.success}`]: {
-    "--toastify-color-success": theme.palette.success.main,
-    "--toastify-text-color-success": theme.palette.success.contrastText,
-    "--toastify-icon-color-success": theme.palette.success.main,
-    "--toastify-color-progress-success": theme.palette.success.main
+    '--toastify-color-success': theme.palette.success.main,
+    '--toastify-text-color-success': theme.palette.success.contrastText,
+    '--toastify-icon-color-success': theme.palette.success.main,
+    '--toastify-color-progress-success': theme.palette.success.main
   },
   [`& .${classes.info}`]: {
-    "--toastify-color-info": theme.palette.info.main,
-    "--toastify-text-color-info": theme.palette.info.contrastText,
-    "--toastify-icon-color-info": theme.palette.info.main,
-    "--toastify-color-progress-info": theme.palette.info.main
+    '--toastify-color-info': theme.palette.info.main,
+    '--toastify-text-color-info': theme.palette.info.contrastText,
+    '--toastify-icon-color-info': theme.palette.info.main,
+    '--toastify-color-progress-info': theme.palette.info.main
   },
   [`& .${classes.error}`]: {
-    "--toastify-color-error": theme.palette.error.main,
-    "--toastify-text-color-error": theme.palette.error.contrastText,
-    "--toastify-icon-color-error": theme.palette.error.main,
-    "--toastify-color-progress-error": theme.palette.error.main
+    '--toastify-color-error': theme.palette.error.main,
+    '--toastify-text-color-error': theme.palette.error.contrastText,
+    '--toastify-icon-color-error': theme.palette.error.main,
+    '--toastify-color-progress-error': theme.palette.error.main
   },
   [`& .${classes.warning}`]: {
-    "--toastify-color-warning": theme.palette.warning.main,
-    "--toastify-text-color-warning": theme.palette.warning.contrastText,
-    "--toastify-icon-color-warning": theme.palette.warning.main,
-    "--toastify-color-progress-warning": theme.palette.warning.main
+    '--toastify-color-warning': theme.palette.warning.main,
+    '--toastify-text-color-warning': theme.palette.warning.contrastText,
+    '--toastify-icon-color-warning': theme.palette.warning.main,
+    '--toastify-color-progress-warning': theme.palette.warning.main
   },
   [`&.${classes.toastWrapper}`]: {
-    borderRadius: "6px",
-    width: "500px",
-    overflowWrap: "anywhere"
+    borderRadius: '6px',
+    width: '500px',
+    overflowWrap: 'anywhere'
+  },
+  [`.Toastify__close-button`]: {
+    background: 'transparent',
+    outline: 'none',
+    border: 'none',
+    padding: 0,
+    cursor: 'pointer',
+    opacity: 1,
+    transition: '0.3s ease',
+    alignSelf: 'auto'
   }
-}));
+}))
 
-export default Container;
+export default Container
